d14641ccd8afffa3e182d21a207c570077396f43
NicMortelliti <npmortelliti@gmail.com>
2022-08-12 12:35:16 -0700

    commit tree b28a57d973f8aabddb965b70f365fb313b57768e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: [:show]
+
   def create
-    user = User.create!(user_params)
+    user = User.create(user_params)
     if user.valid?
       session[:user_id] = user.id
       render json: user, status: :created
@@ -11,14 +13,14 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: session[:user_id])
-    if user
-      render json: user
-    else
-      render json: { error: 'Not authorized' }, status: :unauthorized
-    end
+    render json: user
   end
 
   private
+
+  def authorize
+    return render json: { error: 'Not authorized' }, status: :unauthorized unless session.include? :user_id
+  end
 
   def user_params
     params.permit(:username, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
